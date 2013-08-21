8764fa92bea368faa048f5f0ec6d6cd9aa2f55a2
Guillermo Rauch <rauchg@gmail.com>
2013-08-21 08:49:27 -0700

    initial commit (moved from cloudup repo)

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ mydb-reconnect
 ==============
 
 MyDB automatic reconnection plugin
+
+## How to use
+
+```js
+var db = require('mydb')('/');
+var reconnect = require('mydb-reconnect');
+
+reconnect(db, { connectTimeout: 5000 });
+```
+
+## API
+
+### Reconnect(db, opts)
+
+Options:
+
+- `retryTimeout` how long to wait before reconnection attempts (`10000`)
+- `connectTimeout` how long after a connection attempt to give up (`10000`)
---
+++ index.js
@@ -0,0 +1,155 @@
+
+/**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('cloudup:mydb-reconnect');
+
+/**
+ * Module exports.
+ */
+
+module.exports = Reconnect;
+
+/**
+ * Reconnect mydb.
+ *
+ * @param {mydb#Manager} manager
+ * @api private
+ */
+
+function Reconnect(db, opts){
+  if (!(this instanceof Reconnect)) {
+    return new Reconnect(db, opts);
+  }
+
+  opts = opts || {};
+  this.db = db;
+  this.retryTimeout = opts.retryTimeout || 10000;
+  this.connectTimeout = opts.connectTimeout || 10000;
+
+  // bound methods
+  this.onconnect = this.onconnect.bind(this);
+  this.onerror = this.onerror.bind(this);
+  this.ontimeout = this.ontimeout.bind(this);
+
+  // reconnection state
+  this.reconnecting = false;
+  this.attempts = 0;
+
+  // if we're connecting, set timeout
+  if (!db.connected) this.setConnectTimeout();
+
+  // set connect timeout every time we disconnect
+  db.on('disconnect', this.ondisconnect.bind(this));
+}
+
+/**
+ * Set connect timeout.
+ *
+ * @api private
+ */
+
+Reconnect.prototype.setConnectTimeout = function(){
+  debug('setting connect timeout');
+  this.connectTimer = setTimeout(this.ontimeout, this.connectTimeout);
+  this.db.once('connect', this.onconnect);
+  this.db.once('connect_error', this.onerror);
+};
+
+/**
+ * Called upon connection.
+ *
+ * @api private
+ */
+
+Reconnect.prototype.onconnect = function(){
+  debug('connected');
+  this.attempts = 0;
+  var wasReconnecting = this.reconnecting;
+  this.reconnecting = false;
+  this.cancel();
+  if (wasReconnecting) this.db.emit('reconnect');
+};
+
+/**
+ * Called upon connection timeout.
+ *
+ * @api private
+ */
+
+Reconnect.prototype.ontimeout = function(){
+  debug('timeout (%dms)', this.connectTimeout);
+  this.cancel();
+  this.retry();
+};
+
+/**
+ * Attempts to reconnect.
+ *
+ * @api private
+ */
+
+Reconnect.prototype.retry = function(){
+  if (this.retrying) {
+    console.warn('retry overlap');
+    return;
+  }
+
+  debug('retrying in %dms', this.retryTimeout);
+
+  var self = this;
+  setTimeout(function(){
+    debug('retrying');
+    self.setConnectTimeout();
+    self.db.reconnect();
+  }, this.retryTimeout);
+
+  this.reconnecting = true;
+  this.retrying = true;
+};
+
+/**
+ * Cancel ongoing retry.
+ *
+ * @api private
+ */
+
+Reconnect.prototype.cancel = function(){
+  if (!this.connectTimer) {
+    console.warn('no connect timeout ongoing');
+    return;
+  }
+
+  // in case we were retrying
+  this.retrying = false;
+
+  debug('clearing connect timeout');
+  clearTimeout(this.connectTimer);
+  this.db.off('connect', this.onconnect);
+  this.db.off('connect_error', this.onerror);
+  delete this.connectTimer;
+};
+
+/**
+ * Called upon disconnection.
+ *
+ * @api private
+ */
+
+Reconnect.prototype.ondisconnect = function(){
+  debug('disconnected');
+  this.retry();
+};
+
+/**
+ * Called upon connection error.
+ *
+ * @api private
+ */
+
+Reconnect.prototype.onerror = function(){
+  debug('connection error');
+  this.cancel();
+  this.retry();
+};
